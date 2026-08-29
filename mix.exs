9c31a1404b53e3416432f341a595980a3bc971e1
Abhijit Nalavade <47458252+abhinalavade@users.noreply.github.com>
2026-08-29 14:57:16 +0530

    Update mix.exs
    corrected mix.exs file

--- mix.exs
+++ mix.exs
@@ -5,9 +5,9 @@ defmodule LifeThings.MixProject do
     [
       app: :life_things,
       version: "0.1.0",
-      elixir: "~> 1.14", # Match your local Elixir version
+      elixir: "~> 1.14", 
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps() # Looks for a private or public function named deps/0
     ]
   end
 
@@ -19,7 +19,7 @@ defmodule LifeThings.MixProject do
   end
 
   # Run "mix help deps" to learn about dependencies.
-  def p deps do
+  defp deps do # Fixed: changed 'def p deps do' to 'defp deps do'
     [
       # Add your dependencies here, for example:
       # {:phoenix, "~> 1.7.0"},
